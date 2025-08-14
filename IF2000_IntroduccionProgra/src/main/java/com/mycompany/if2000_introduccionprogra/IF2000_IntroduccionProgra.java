d52743477b2efa5630a46d475cef47ccb1effa88
letyavyespinoza <letyavy.jo.espinozarivas@gmail.com>
2025-08-14 11:23:53 -0600

    Mi primer Commit en Java

---
+++ IF2000_IntroduccionProgra/src/main/java/com/mycompany/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.if2000_introduccionprogra;
+
+/**
+ *
+ * @author USER
+ */
+public class IF2000_IntroduccionProgra {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        
+        String name = "Lionel";
+        int age = 40;
+        
+        System.out.println("Hello World!");
+        System.out.println("Name: " +name);
+        System.out.println("Age: " +age);
+        //Python
+        //print("Hola mundo")
+        
+        /*Tipos de datos numericos:
+        int, long, short, float, double, 
+        numeric, money, decimal
+        
+        Menejo de fechas: 
+        Date
+        
+        Cadenas de carecteres: 
+        String, char
+        
+        Objetos: 
+        Object
+        
+        Data Structures:
+        Arrays or Vectors: int[] x;
+                           int[][] y;
+        True/False:
+        boolean
+        */
+        
+    }
+}
---
+++ IF2000_IntroduccionProgra/src/main/java/domain/Person.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+
+public class Person {
+    
+    //Atributes
+    private String name; 
+    private String lastName;
+    private String id;
+    private String phone; 
+    private int age; 
+
+    public Person(String name, String lastName, String id, String phone, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person\n"
+                + "name=" + getName() 
+                + "\n, lastName=" + getLastName() 
+                + "\n, id=" + getId()
+                + "\n, phone=" + getPhone() 
+                + "\n, age=" + getAge();
+    }
+    
+    
+    
+}
